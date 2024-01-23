757c79e5fd382036dc66ad47ae299e09ece5bf3d
marmulle <markfelixm@gmail.com>
2024-01-23 17:54:15 +0200

    fix: linkedin icon now .svg, svgs now black

--- src/lib/ExternalNavigation.svelte
+++ src/lib/ExternalNavigation.svelte
@@ -1,17 +1,17 @@
 <script>
-	import github_svg from '$lib/assets/github-mark.svg';
-	import linkedin_png from '$lib/assets/linkedin.webp';
+	import github_icon from '$lib/assets/github-mark.svg';
+	import linkedin_icon from '$lib/assets/linkedin.svg';
 </script>
 
 <ul class="links">
 	<li class="github">
 		<a href="https://github.com/Markfelixm" target="_blank" rel="noopener noreferrer">
-			<img src={github_svg} alt="Github profile link" />
+			<img src={github_icon} alt="Github profile link" />
 		</a>
 	</li>
 	<li class="linkedin">
 		<a href="https://www.linkedin.com/in/markfelixmuller" target="_blank" rel="noopener noreferrer">
-			<img src={linkedin_png} alt="LinkedIn profile link" />
+			<img src={linkedin_icon} alt="LinkedIn profile link" />
 		</a>
 	</li>
 </ul>
@@ -22,10 +22,11 @@
 		box-sizing: border-box;
 		margin: 0;
 		padding-inline: 0;
-		padding-block: 1rem;
+		padding-block: 2rem;
 		display: grid;
 		grid-template-columns: 1fr 1fr;
 		justify-items: center;
+		place-content: center;
 		gap: 2rem;
 	}
 	a {
@@ -33,6 +34,7 @@
 		color: black;
 	}
 	img {
+		display: block;
 		max-height: 50px;
 		width: auto;
 	}
